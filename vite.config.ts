1e446c45dc173d729971be96355de444488977e9
Janapati Venkata Sriveda Manaswi <165014242+manas-janapati@users.noreply.github.com>
2025-04-24 14:18:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/movie-genre-frontend/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
